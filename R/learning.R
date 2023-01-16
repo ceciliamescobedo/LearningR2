3d952d8d6a495258bb81e0825ab5546cc990cc56
Cecilia Martinez Escobedo <martinez.hildacecilia@gmail.com>
2023-01-16 16:00:43 +0100

    Add R file

---
+++ R/learning.R
@@ -0,0 +1,48 @@
+10
+
+# R basics ----------------------------------------------------------------
+
+# Assignment of variables
+weight_kilos <- 100
+weight_kilos <- 10
+weight_kilos
+colnames(airquality)
+
+# Character vector
+c("a", "b", "c")
+# Logic vector
+c(TRUE, FALSE, FALSE)
+# Numeric vector
+c(1, 5, 6)
+# Factor vector
+factor(c("low", "high", "medium", "high"))
+
+# Looking at datasets
+
+# what is available in a dataset
+# str() is the function to see the “object structure”
+str(airquality)
+# basic summary of dataset
+summary(airquality)
+
+head(airquality)
+
+# Object names
+2 + 2
+DayOne
+T <- FALSE
+c <- 9
+
+# Spacing
+x[, 1]
+x[, 1]
+mean(x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+height <- feet * 12 + inches
+df$z
+x <- 1:10
+
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
